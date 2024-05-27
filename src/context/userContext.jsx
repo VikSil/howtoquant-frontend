dae3e13c46a805410ac9b35e3adcd0e7f4b77612
Viktoria Silver <viktoria.sudraba@gmail.com>
2024-05-27 03:05:03 +0100

    Amend version number

--- src/context/userContext.jsx
+++ src/context/userContext.jsx
@@ -3,7 +3,7 @@ import { createContext, useState } from 'react';
 export const UserContext = createContext();
 
 export const UserProvider = ({ children }) => {
-    const [user, setUser] = useState({'username':"Anonymous", "organisation":"Public Domain", "version": "0.1.0-alpha" });
+    const [user, setUser] = useState({'username':"Anonymous", "organisation":"Public Domain", "version": "0.2.0" });
   
     return (
       <UserContext.Provider value={{ user, setUser }}>
