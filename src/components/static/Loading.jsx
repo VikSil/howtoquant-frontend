b089fa731db29894a8cc2ea66523a1d5d14061dd
Adarsh Kumar <adarsh010c@gmail.com>
2024-10-05 03:40:35 +0530

    remove spinner-react, add pure css loader (issue #1)
    * removed spinner-reac, added pure css loader
    
    * removed the green loade component

--- src/components/static/Loading.jsx
+++ src/components/static/Loading.jsx
@@ -1,10 +1,10 @@
-import {SpinnerInfinity} from 'spinners-react'
+import '../../assets/css/Loader.css';
 
 export default function Loading(){
     return (
         <main className="mx-5 mt-4 ">
             <p className="text-center mb-4">Loading, please wait...</p>     
-            <SpinnerInfinity size={60} thickness={100} speed={100} color="#556b2f" secondaryColor="rgba(85,107,47,0.3)" />
+            <div className='spinner' ></div>
         </main>        
     )
 }
